c603b25ddb7a2a50dc07c041f515d0a5bc40bdcf
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-15 19:43:55 +0300

    Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num > 0)
+{        
+    sum += num % 10;
+    num = num / 10;
+}
+Console.WriteLine(sum);
