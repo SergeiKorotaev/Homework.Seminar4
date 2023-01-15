59ab5634eff1fb29c2f8a5b83e6f481d9941c759
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-15 19:42:56 +0300

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for(int i = 0; i <= (number2 -1); i++)
+{
+    result *= number1;
+}
+Console.Write(result);
