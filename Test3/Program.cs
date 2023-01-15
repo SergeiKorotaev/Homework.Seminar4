0ce7d6c4a7d19d35a318399d720f12daa158e978
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-15 19:44:13 +0300

    Test3

---
+++ Test3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите колличество элементов в массиве: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[num];
+Random A = new Random();
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = A.Next(0,100);
+}
+Console.WriteLine(string.Join(" ", number));
